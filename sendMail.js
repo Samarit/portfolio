11bdc9de97adb338d8810565d6878968eb7e594b
Samarit <kislyakaleksey@gmail.com>
2022-06-22 00:05:20 +0300

    .env ignored

--- sendMail.js
+++ sendMail.js
@@ -3,8 +3,44 @@ const { google } = require('googleapis')
 const OAuth2 = google.auth.OAuth2
 require('dotenv').config()
 
+const createTransporter = async () => {
+  const oauth2client = new OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  )
+
+  oauth2client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN
+  })
+
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2client.getAccessToken((err, token) => {
+      if (err) {
+        reject()
+      }
+      resolve(token)
+    })
+  })
+
+  let auth = {
+    type: "OAuth2",
+    user: process.env.EMAIL,
+    accessToken,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN
+  }
+
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: auth
+  })
+
+  return transporter
+}
+
 const sendMail = async ( data = { name: '', email: '', text: ''} ) => {
-  
   
   try {
     //Test account
@@ -13,20 +49,10 @@ const sendMail = async ( data = { name: '', email: '', text: ''} ) => {
       user: testAccount.user,
       pass: testAccount.pass
     }
-    let auth = {
-      user: 'samarit.bot@gmail.com',
-      pass: 'Prikol1993',
-      
-    }
 
-    console.log(`authTest: ${authTest.user}, ${authTest.pass}`)
+    let emailTransporter = await createTransporter()
 
-    let transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: auth
-    })
-
-    let status = await transporter.sendMail({
+    let status = await emailTransporter.sendMail({
       from: `${data.name} <${data.email}>`,
       to: "warlockultras2008@yandex.ru",
       subject: "Test mail",
@@ -39,7 +65,7 @@ const sendMail = async ( data = { name: '', email: '', text: ''} ) => {
     return status
 
   } catch(err) {
-    console.log(new Error(err))
+    console.log(new Error(err), 'here?')
   }
 }
 
