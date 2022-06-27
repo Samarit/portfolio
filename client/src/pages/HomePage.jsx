9004e2e2dcd59d89804393a17edd735e12de40fb
Samarit <kislyakaleksey@gmail.com>
2022-06-27 22:15:29 +0300

    main button

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,12 +4,15 @@ import MainPage from "./MainPage";
 export default function HomePage() {
   return (
     <div className="content-wrapper">
+
       <MainPage />
-       {routes.map((route) => {
-         if (route.path === "/") {
-           return null
-          } else return <route.Component />
-       })}
+
+      {routes.map((route) => {
+        if (route.path === "/") {
+          return null
+         } else return <route.Component />
+      })}
+      
     </div>
   )
 }
--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,10 +1,13 @@
-import routes from "../routes"
+import { Link } from "react-router-dom"
+import '../css/MainPage.sass'
 
 export default function MainPage() {
   
   return (
         <div className="page">
-          <h1>MAIN</h1>
+          <h1>HELLO!</h1>
+          <h2>I'm Aleksey, web-developer</h2>
+          <Link to={'/contacts'} id='mainButton'>Contact me!</Link>
         </div>
   )
 }
